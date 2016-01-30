ddddac415117ecffb06f83973e123f877670ea0e
Manu Dwivedi <manu29.d@gmail.com>
2016-01-30 12:40:57 +0530

    Initial Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ibmhackathonApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ibmhackathonApp
+ */
+angular.module('ibmhackathonApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
